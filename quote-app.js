3aac89f0d6630868cc5c9c4ebd447f4a74031ec5
Christopher Li <chrisli41@gmail.com>
2016-09-20 16:19:43 -0700

    Initial Commit

---
+++ quote-app.js
@@ -0,0 +1,45 @@
+/**
+ * Created by christopherli on 9/16/16.
+ */
+function randomize(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function success(data){
+    var quoteText = data.quoteText;
+    var quoteAuthor = data.quoteAuthor === '' ? 'Unknown' : data.quoteAuthor;
+
+    var link = 'http://twitter.com/share?text=' + quoteText;
+
+    $('#text').text(quoteText);
+    $('#author').text("- " + quoteAuthor);
+    $('#share').attr('href', link);
+}
+
+$(document).ready(function(){
+
+    var colors = ['#0099cc', '#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+    var apiCall = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
+
+    $.getJSON(apiCall, success);
+
+    $('#new-quote').click(function(){
+
+        var color = randomize(0, colors.length - 1);
+
+        $.getJSON(apiCall, success);
+        
+        $("html body").animate({
+            backgroundColor: colors[color],
+            color: colors[color]
+        }, 1000);
+
+        $("#new-quote, #twit").animate({
+            backgroundColor: colors[color]
+        }, 1000);
+
+        }
+    );
+
+});
